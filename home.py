1882e328998d6a05cfac0967b2bb5b981c304875
SWatcharapol <119186357+SWatcharapol@users.noreply.github.com>
2023-04-06 16:06:52 +0700

    .commit

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,18 +6,19 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.image("./img/soiju.jpg")
-
+st.image("./img/iris.png")
 html_8 = """
-<div style="background-color:#ffffcc;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>ทำนายข้อมูล</h5></center>
+<div style="background-color:#990066;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
 </div>
 """
+
 st.markdown(html_8, unsafe_allow_html=True)
 st.markdown("")
 
-
 dt = pd.read_csv("./data/iris.csv")
+
+
 st.write(dt.head(10))
 
 dt1 = dt['petal.length'].sum()
@@ -28,14 +30,14 @@ dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
 
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   #st.write(dt.head(10))
+   st.write(dt.head(10))
    st.bar_chart(dx2)
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
 
 html_8 = """
-<div style="background-color:#ffffcc;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
 """
@@ -48,7 +50,14 @@ sp_len = st.number_input("กรุณาเลือกข้อมูล sepal
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
 
 if st.button("ทำนายผล"):
-   st.markdown("ใส่โมเดล")
+
+   X = dt.drop('variety', axis=1)
+   y = dt.variety
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)
+   x_input = np.array([[5.1, 3.5, 1.4, 0.2]])
+   st.write(Knn_model.predict(x_input))
+
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
