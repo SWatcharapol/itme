1fe14cf333c7d91c61b7b4d398d12d365987456c
SWatcharapol <119186357+SWatcharapol@users.noreply.github.com>
2023-04-06 15:25:28 +0700

    Update home.py

--- home.py
+++ home.py
@@ -7,11 +7,3 @@ import plotly.graph_objects as go
 
 
 st.head("soiju")
-html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
-</div>
-"""
-
-st.markdown(html_8, unsafe_allow_html=True)
-st.markdown("")
