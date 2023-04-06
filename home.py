4b1361d604815b34606733055caec215f84c3edf
SWatcharapol <119186357+SWatcharapol@users.noreply.github.com>
2023-04-06 15:24:31 +0700

    Update home.py

--- home.py
+++ home.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-st.image("./img/soiju.jpg")
+st.head("soiju")
 html_8 = """
 <div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>การทำนายข้อมูลดอกไม้</h5></center>
