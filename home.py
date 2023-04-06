9508d273dd6522bb5a649ad0cd9759dd5181b6a3
SWatcharapol <119186357+SWatcharapol@users.noreply.github.com>
2023-04-06 15:50:37 +0700

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ st.header("watcharpol")
 st.image("./img/soiju.jpg")
 
 html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#ffffcc;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
 """
@@ -29,14 +29,14 @@ dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
 
 if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   st.write(dt.head(10))
+   #st.write(dt.head(10))
    st.bar_chart(dx2)
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
 
 html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#ffffcc;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
 """
