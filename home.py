acac19c22069aceff1c073299a7eaabe6bc05c8b
SWatcharapol <119186357+SWatcharapol@users.noreply.github.com>
2023-04-06 16:32:03 +0700

    .commit

--- home.py
+++ home.py
@@ -63,7 +63,7 @@ if st.button("ทำนายผล"):
    out = Knn_model.predict(x_input)
 
    if out[0] == 'Setosa':
-    st.image("./pic/soiju.jpg")
+    st.image("./pic/setosa.jpg")
    else:
     st.writ('xxx')
    # st.button("ไม่แสดงข้อมูล")
