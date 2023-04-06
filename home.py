dc9eb5c8ec0ee6359ad942276f37142c96040194
SWatcharapol <119186357+SWatcharapol@users.noreply.github.com>
2023-04-06 15:33:42 +0700

    .commit

--- home.py
+++ home.py
@@ -5,5 +5,13 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
+# st.header("Patcharaphon")
+st.image("./img/soiju.jpg")
 
-st.head("soiju")
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
